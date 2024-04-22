9756d6f683dd0ae584bbb872b233f440de416b2f
Nazar <109137944+NazGo0lm@users.noreply.github.com>
2024-04-22 20:55:59 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,55 +1,22 @@
-function calcAverageCalories(days){
-    let theArray = Object.values(days);
-    let theSum = 0;
-    for (let theValue of theArray) {
-        //console.log(theValue.calories);
-        //console.log(theArray.length);
-        let supValue = theValue.calories;
-        theSum += supValue
+function makeArray(firstArray, secondArray, maxLength){
+    let theArray = [];
+    let combinedArray = theArray.concat(firstArray, secondArray);
+    //console.log(combinedArray);
+    let maxedArray = combinedArray.slice(0, maxLength);
+    //console.log(maxedArray);
+    // console.log(combinedArray.length); 
+    if (maxLength <= combinedArray.length) {
+        
+        //console.log("ok");
+        return maxedArray;
+    } else {
+        return []
     }
-    if (theArray.length >0) {
-        return theSum/theArray.length
-    }
-    return 0
 }
 
-
-
-
-
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
-
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
-
-console.log(
-  calcAverageCalories([])
-); // 0
-
-/* console.log(
-  calcAverageCalories([{ day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },])
-); // 2155
-console.log(
-  calcAverageCalories([{ day: "monday", calories: 2040 },
-    ])
-); // 2040 */
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"] 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
